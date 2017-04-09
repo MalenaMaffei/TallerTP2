14ca159e4f261386649b027704ee5d73a619d54b
MalenaMaffei <malenamaffei@hotmail.com>
2017-04-09 20:05:25 -0300

    prueba malisima

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 //
 
 #include <cstdio>
+#include <string>
+#include <iostream>
 #include "Packet.h"
+
+using namespace std;
 int main(){
-    printf("%s", "hola");
+    // printf("%s", "hola");
+    Packet p = Packet(1,2,3);
+    p.setData("Hola soy un paquetito");
+    string s = p.getData();
+    cout << s << endl;
+
     return 0;
 }
